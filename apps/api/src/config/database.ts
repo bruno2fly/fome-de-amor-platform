5847df3de9411517675709c2011727c4b917bf64
bruno2fly <2flydigitalmarketing@gmail.com>
2026-08-12 00:27:34 -0400

    Enable SSL for non-local Postgres connections (Render requires SSL)

--- apps/api/src/config/database.ts
+++ apps/api/src/config/database.ts
@@ -1,6 +1,9 @@
 import { Pool } from 'pg';
 import { env } from './env';
 
+const isLocalDb = /localhost|127\.0\.0\.1/.test(env.databaseUrl);
+
 export const db = new Pool({
-  connectionString: env.databaseUrl
+  connectionString: env.databaseUrl,
+  ssl: isLocalDb ? false : { rejectUnauthorized: false }
 });
